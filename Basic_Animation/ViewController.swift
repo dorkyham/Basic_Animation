ed57f5fd706ad0b6d1b66f318cc0e51821079680
Annisa Nabila Nasution <annisa.nasution@binus.ac.id>
2019-05-17 16:07:48 +0700

    Belajar

--- Basic_Animation/ViewController.swift
+++ Basic_Animation/ViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var rectangle: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        UIView.animate(withDuration: 1, delay: 0, animations: {
+            
+            self.rectangle.frame = CGRect(x: 30, y: 30, width: 100, height: 100)
+            
+        })
     }
 
 
